56c50ab107598f6981674b70ecc3e7a2f5138b19
Jack Schluger <jschluger@stuy.edu>
2017-05-20 16:14:46 -0400

    working

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 #imports
 from flask import Flask, render_template
 
-#create a flask app (secret key created when initialize.py is run)
 app = Flask(__name__)
-f = open( "utils/key", 'r' )
-app.secret_key = "hello" #f.read();
-f.close()
 
-#the default page is allows you to choose to login or register
 @app.route("/")
-#PLACEHOLDER CODE ASHER AND CHLOE WILL WORK ON THIS 
-#def loginOrRegister():
-#    if "status" in session:
-#        session.pop("status")
-#    if 'username' in session:
-#        return render_template("index.html",username = session['username'])
-#    else:
-#        return render_template("index.html")
-        
+def home():
+    return render_template("index.html")
+
+
 #run the app
 if __name__ == "__main__":
     app.debug = True
