effc049fae0769304d29cef05e6d23808fd3064b
Jack Schluger <jschluger@stuy.edu>
2017-05-23 13:48:05 -0400

    making map work w what we had

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-$(document).foundation()
+3$(document).foundation()
 console.log("hello world");
 
 //this div will contain a list of 10 svg's, each of which will be a map of one floor
@@ -6,21 +6,23 @@ var map = document.getElementById("map");
 
 var i;
 for (i = 1; i <= 10; i++){
-    console.log("next svg");
-    var newSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    newSvg.id = i;
-    newSvg.setAttribute('height', 400);
-    newSvg.setAttribute('width', 200);
-    if (i == 1)
-	newSvg.setAttribute('display', 'inline');
-    else
-	newSvg.setAttribute('display', 'none');
-    var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-    text.setAttribute('x', 10 * i);
-    text.setAttribute('y', 37 * i);
-    text.innerHTML = "floor " + i;
-    newSvg.append( text );
-    map.append( newSvg );
+    if (id - 7) {
+	console.log("next svg");
+	var newSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+	newSvg.id = i;
+	newSvg.setAttribute('height', 400);
+	newSvg.setAttribute('width', 200);
+	if (i == 1)
+	    newSvg.setAttribute('display', 'inline');
+	else
+	    newSvg.setAttribute('display', 'none');
+	var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', 10 * i);
+	text.setAttribute('y', 37 * i);
+	text.innerHTML = "floor " + i;
+	newSvg.append( text );
+	map.append( newSvg );
+    }
 }
 
 
