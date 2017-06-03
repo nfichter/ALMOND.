b41fa3c22bc0dbec56a67a5cbac921bfbfd0c092
cdelfau <cdelfau@stuy.edu>
2017-06-03 18:47:38 -0400

    hover functionality almost done, click not yet

---
+++ static/js/mouse.js
@@ -0,0 +1,50 @@
+//var floordata = "coords.txt";
+var floor0, floor1, floor2, floor3, floor4, floor5, floor6, floor7, floor8, floor9, floor10, dict;
+floor0 = "";
+floor1 = "";
+floor2 = "";
+floor3 = "";
+floor4 = "";
+floor5 = "";
+floor6 = "";
+floor7 = "739: 156 126 78 48\n 737: 156 176 78 48\n 735: 156 226 78 48\n 731: 156 276 78 88\n 729: 156 366 78 28\n 727: 156 396 78 48\n 717: 156 446 48 48\n 715: 156 541 38 28\n 709: 156 571 38 78\n 707: 156 651 38 48\n 705: 156 701 38 48\n 703: 156 751 78 84\n 701: 236 751 78 42\n 702: 316 694 38 55\n 704: 316 636 38 55\n 726: 296 381 58 23\n 736: 296 281 58 58\n 738: 296 221 58 58\n 740: 296 161 58 58\n ES: 316 601 38 33\n WS: 296 406 58 33\n HS: 296 126 58 33\n EL: 196 571 38 38\n ESC: 316 471 38 98\n M: 296 341 58 18\n W: 397 361 58 18";
+floor8 = "839: 156 126 78 48\n 837: 156 176 78 48\n 835: 156 226 78 48\n 831: 156 276 78 88\n 829: 156 366 78 28\n 827: 156 396 78 48\n 817: 156 446 48 48\n 811: 156 571 38 78\n 809: 156 651 38 48\n 803: 156 701 50 105\n 801: 206 750 44 56\n 802: 250 750 41 56\n 804: 316 635 39 114\n 826: 296 381 58 23\n 836: 296 281 58 58\n 838: 296 221 58 58\n 840: 296 161 58 58\n ES: 316 601 38 33\n WS: 296 406 58 33\n HS: 296 126 58 33\n EL: 196 571 38 38\n ESC: 316 471 38 98\n M: 296 341 58 18\n W: 397 361 58 18";
+floor9 = "";
+floor10 = "";
+
+var dict = {}
+dict.flr0 = floor0;
+dict.flr1 = floor1;
+dict.flr2 = floor2;
+dict.flr3 = floor3;
+dict.flr4 = floor4;
+dict.flr5 = floor5;
+dict.flr6 = floor6;
+dict.flr7 = floor7;
+dict.flr8 = floor8;
+dict.flr9 = floor9;
+dict.flr10 = floor10;
+
+f = document.getElementById("current_floor"); 
+flr = f.value; //gives string of the floor number (aka value)
+//flrnbr = "floor" + flr; //returns "floor#"
+print floor7;
+
+function getFloorData(flrnbr) {
+    var getCoords = floordata[0];
+    alert("Floor..." + getCoords);
+    browser.setAttributeNS(xlinkNS, "href", getCoords);
+    return 
+}
+
+//function hover(mouseX, mouseY, floor) {
+//    
+//}
+
+//function click(mouseX, mouseY) {
+//place the room in the get directions function
+//document.getElementById("myBtn").addEventListener("click", displayDate);
+//    return 
+//}
+
+//print getFloorData(flrnbr);
